d67678d417b50e81f01c4b64561ddba897989b75
Sathya Swaruban <sathya.swaruban@outlook.com>
2023-10-28 18:45:18 +0530

    Update Assignment_07

--- src/Assignments/Assignment_07.java
+++ src/Assignments/Assignment_07.java
@@ -1,10 +1,19 @@
 package Assignments;
+/*
+Implement a program to find and display the least common multiple (LCM) of two whole numbers.
 
+Least Common Multiple (LCM) of two numbers, num1 and num2 is the smallest positive number that is divisible by both
+num1 and num2.
+
+Example:
+    Input:  7 and 9
+    Output: 63
+ */
 public class Assignment_07 {
     public static void main(String[] args) {
         int  number1 = 7, number2 = 9;
         int lcm = (number1 * number2) / gcd(number1, number2);
-        System.out.println("The LCM of " + number1 + " and " + number2 + " is " + lcm);
+        System.out.println(lcm);
     }
     static int gcd(int number1, int number2) {
         while (number2 != 0) {
