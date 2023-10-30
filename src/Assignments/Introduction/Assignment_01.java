30deb267eb37f8a4a11d26ea00b555d2c6db9ef9
Sathya Swaruban <sathya.swaruban@outlook.com>
2023-10-30 15:36:16 +0530

    Create introduction assignment 1

---
+++ src/Assignments/Introduction/Assignment_01.java
@@ -0,0 +1,15 @@
+package Assignments.Introduction;
+/*
+Create a new Java program to display a postal address.
+
+Example:
+    Door No: D089
+    Street: St. Louis Street
+    City: Springfield
+    ZIP Code: 62729
+*/
+public class Assignment_01 {
+    public static void main(String[] args) {
+        System.out.println("Door No: D089\nStreet: St. Louis Street\nCity: Springfield\nZIP Code: 62729");
+    }
+}
