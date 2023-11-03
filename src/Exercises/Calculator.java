ab8464ca6967b61c5ee3025238c194923cab452a
Sathya Swaruban <sathya.swaruban@outlook.com>
2023-11-03 13:51:35 +0530

    Create/Update remaining exercises

--- src/Exercises/Calculator.java
+++ src/Exercises/Calculator.java
@@ -1,6 +1,20 @@
 package Exercises;
 
 class Calculator {
+
+    public int num;
+
+    public int sumOfDigits() {
+        int sum = 0;
+        int temp = this.num;
+        while (temp != 0) {
+            int remainder = temp % 10;
+            sum += remainder;
+            temp = temp / 10;
+        }
+        return sum;
+    }
+
     public double findAverage(int number1, int number2, int number3) {
         double average = (double) (number1 + number2 + number3) / 3.0;
         return Math.round(average * 100.0) / 100.0;
@@ -8,9 +22,10 @@ class Calculator {
 }
 
 class Tester2 {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        double result = calculator.findAverage(12, 8, 15);
-        System.out.println(result);
+        calculator.num = 6547;
+        System.out.println(calculator.sumOfDigits());
+        System.out.println(calculator.findAverage(12, 8, 15));
     }
 }
